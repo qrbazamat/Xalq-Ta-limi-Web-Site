501d02e8461319b10eb0d9f1c91d09c8e74efa62
AzamatCoder <60395192+qrbazamat@users.noreply.github.com>
2022-07-28 18:08:51 +0500

    Delete js directory

--- js/utility.js
+++
@@ -1,9 +0,0 @@
-const $ = selector => document.querySelector(selector)
-const $$ = selector => document.querySelectorAll(selector)
-
-const createElement = (elementName, className) => {
-    const element = document.createElement(elementName);
-    className && (element.className = className);
-
-    return element;
-}
